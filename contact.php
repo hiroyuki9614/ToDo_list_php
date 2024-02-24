e7886a3523cd6902c94c4de1cfafeae3387381d1
hiroyuki9614 <mmnnoov@gmail.com>
2024-02-24 11:33:40 +0900

    EC practice

---
+++ contact.php
@@ -0,0 +1,9 @@
+<?php
+
+if ($_POST) {
+	if ($_POST) {
+		echo "HTMLからPOST送信を受け取りました";
+	} else {
+		echo "HTMLからのPOST送信受信に失敗しました";
+	}
+}
---
+++ staff_add.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="utf-8" />
+	<title>茶葉の園</title>
+</head>
+
+<body>
+	<p>スタッフ追加</p><br>
+	<br>
+	<form method="post" action="staff_add_check.php">
+		<label for="user_name">ユーザー名</label><br>
+		<input type="text" name="name" id="user_name" style="width: 200px;"><br>
+		<label for="user_pass">パスワード</label><br>
+		<input type="password" name="pass" id="user_pass" style="width: 100px;"><br>
+		<label for="user_pass2">パスワードをもう一度入力して下さい。</label><br>
+		<input type="password" name="pass2" id="user_pass2" style="width: 100px;"><br>
+		<br>
+		<button onclick="history.back()">戻る</button>
+		<button type="submit">OK</button>
+	</form>
+</body>
+
+</html>
