56c497c695851287197b8850fbe8059d912a1fef
hiroyuki9614 <mmnnoov@gmail.com>
2024-02-27 21:33:11 +0900

    add product

--- product/pro_delete.php
+++ product/pro_delete.php
@@ -18,27 +18,35 @@
 		$dbh = new PDO($dsn, $user, $password);
 		$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-		$sql = 'SELECT name FROM mst_product WHERE code=?';
+		$sql = 'SELECT name,gazou FROM mst_product WHERE code=?';
 		$stmt = $dbh->prepare($sql);
 		$data[] = $pro_code;
 		$stmt->execute($data);
 
 		$rec = $stmt->fetch(PDO::FETCH_ASSOC);
 		$pro_name = $rec['name'];
+		$pro_gazou_name = $rec['gazou'];
 
 		$dbh = null;
+		if ($pro_gazou_name == '') {
+			$disp_gazou = '';
+		} else {
+			$disp_gazou = '<img src="./gazou/ ' . $pro_gazou_name . '">';
+		}
 	} catch (Exception $e) {
 		print 'ただいま障害により大変ご迷惑をお掛けしております。';
 		exit();
 	}
 
 	?>
-	<p>スタッフを削除する</p>
-	<p>スタッフ名:<?php print $pro_name ?></p>
-	<p>スタッフコード</p>
+	<p>商品を削除する</p>
+	<p>商品名:<?php print $pro_name ?></p>
+	<p>商品コード</p>
 	<?php print $pro_code; ?>
 	<form method="post" action="pro_delete_done.php">
 		<input type="hidden" name="code" value="<?php print $pro_code; ?>">
+		<?php print $disp_gazou; ?><br>
+		<input type="hidden" name="gazou_name" value="<?php print $pro_gazou_name; ?>">
 		<input type="button" onclick="history.back()" value="戻る">
 		<button type="submit">OK</button>
 	</form>
