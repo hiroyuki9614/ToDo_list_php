22be4c1913928d9862513d1e19dc6f6037171228
hiroyuki9614 <mmnnoov@gmail.com>
2024-02-29 19:31:44 +0900

    add stuff

---
+++ staff_login/staff_top.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="utf-8" />
+	<title>茶葉の園</title>
+</head>
+
+<body>
+	<h2>ショップ管理トップメニュー</h2><br>
+	<a href="../staff/staff_list.php">スタッフ管理</a><br>
+	<a href="../product/pro_list.php">商品管理</a><br>
+</body>
