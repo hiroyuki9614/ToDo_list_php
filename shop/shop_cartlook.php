2fe831290baef65ed059875362e2a235bbbd94d7
hiroyuki9614 <mmnnoov@gmail.com>
2024-03-03 21:34:26 +0900

    add function of shopping cart

--- shop/shop_cartlook.php
+++ shop/shop_cartlook.php
@@ -27,10 +27,20 @@ if (isset($_SESSION['member_login']) == false) {
 	<?php
 
 	try {
-		$cart = $_SESSION['cart'];
-		$kazu = $_SESSION['kazu'];
-		$max = count($cart);
+		if (isset($_SESSION['cart']) == true) {
+			$cart = $_SESSION['cart'];
+			$kazu = $_SESSION['kazu'];
+			$max = count($cart);
+		} else {
+			$max = 0;
+		}
 
+		if ($max == 0) {
+			print '<p>カートに商品が入っていません。</p><br>';
+			print '<br>';
+			print '<a href="shop_list.php">商品一覧へ戻る</a>';
+			exit();
+		}
 		$dsn = 'mysql:dbname=shop;host=localhost;charset=utf8';
 		$user = 'root';
 		$password = '';
@@ -63,17 +73,32 @@ if (isset($_SESSION['member_login']) == false) {
 
 	?>
 	<p>カートの中身</p><br>
-	<?php for ($i = 0; $i < $max; $i++) {
-	?>
-		<?php print $pro_name[$i]; ?>
-		<?php print $pro_gazou[$i]; ?>
-		<?php print $pro_price[$i] . '円'; ?>
-		<?php print $kazu[$i]; ?>
-		<?php print '<br>'; ?>
-	<?php
-	}
-	?>
 	<form method="post" action="kazu_change.php">
+		<table border="1">
+			<tr>
+				<td>商品</td>
+				<td>商品画像</td>
+				<td>価格</td>
+				<td>数量</td>
+				<td>小計</td>
+				<td>削除</td>
+			</tr>
+			<?php for ($i = 0; $i < $max; $i++) {
+			?>
+				<tr>
+					<td><?php print $pro_name[$i]; ?></td>
+					<td><?php print $pro_gazou[$i]; ?></td>
+					<td><?php print $pro_price[$i] . '円'; ?></td>
+					<td><input type="text" name="kazu<?php print $i; ?>" value="<?php print $kazu[$i]; ?>"></td>
+					<td><?php print $pro_price[$i] * $kazu[$i] . '円'; ?></td>
+					<td><input type="checkbox" name="sakujo<?php print $i ?>"></td>
+				</tr>
+			<?php
+			}
+			?>
+		</table>
+		<input type="hidden" name="max" value="<?php print $max; ?>">
+		<button type="submit">数量変更</button>
 		<input type="button" onclick="history.back()" value="戻る">
 	</form>
 
