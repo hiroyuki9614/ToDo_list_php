19b1df1022e6f9ead35fcb52474234f94cadf5da
hiroyuki9614 <mmnnoov@gmail.com>
2024-03-07 18:08:30 +0900

    add shopping form

---
+++ shop/shop_form_done.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="utf-8" />
+	<title>茶葉の園</title>
+</head>
+
+<body>
+	<?php
+
+	require_once('../common/common.php');
+
+	$post = sanitize($_POST);
+
+	$onamae = $post['onamae'];
+	$email = $post['email'];
+	?>
+</body>
