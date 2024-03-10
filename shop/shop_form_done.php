bcb8ab250095972ec9f54891f9595a84a5f4677b
hiroyuki9614 <mmnnoov@gmail.com>
2024-03-10 21:40:13 +0900

    add shopping form

--- shop/shop_form_done.php
+++ shop/shop_form_done.php
@@ -1,3 +1,7 @@
+<?php
+session_start();
+session_regenerate_id(true);
+?>
 <!DOCTYPE html>
 <html>
 
@@ -9,20 +13,31 @@
 <body>
 	<?php
 
-	require_once('../common/common.php');
+	try {
+		require_once('../common/common.php');
 
-	$post = sanitize($_POST);
-	
-	$onamae = $post['onamae'];
-	$email = $post['email'];
-	$postal1 = $post['postal1'];
-	$postal2 = $post['postal2'];
-	$address = $post['address'];
-	$tel = $post['tel'];
+		$post = sanitize($_POST);
 
-	print $onamae. '様<br>';
+		$onamae = $post['onamae'];
+		$email = $post['email'];
+		$postal1 = $post['postal1'];
+		$postal2 = $post['postal2'];
+		$address = $post['address'];
+		$tel = $post['tel'];
 
-	$onamae = $post['onamae'];
-	$email = $post['email'];
+		print $onamae . '様<br>';
+
+		print '<p>ご注文ありがとうございました。</p>';
+		print '<p>' . $email . '宛にメールを送付しました。</p>';
+		print '<p>商品は下記住所に配送されます。</p>';
+		print '<p>' . $postal1 . '-' . $postal2 . '</p>';
+		print '<p>' . $address . '</p>';
+		print '<p>' . $tel . '</p>';
+	} catch (Exception $e) {
+		print 'ただいま障害により大変ご迷惑をお掛けしております。';
+		print $e . '<br>';
+		print $e->getMessage();
+		exit();
+	}
 	?>
 </body>
